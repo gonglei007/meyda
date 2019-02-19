c5932515b869948f4dc5f5bee94d999fbb7795db
renyunhao <renyunhao@gmail.com>
2019-02-19 23:19:15 +0800

    屏蔽因为构造传参AudioContext与Source为null产生的报错

--- src/meyda-wa.js
+++ src/meyda-wa.js
@@ -29,14 +29,16 @@ export class MeydaAnalyzer {
   constructor(options, _this) {
     this._m = _this;
     if (!options.audioContext) {
-      throw this._m.errors.noAC;
+      //因为Linux机器上无法生成AudioContext，因此直接计算，将这类错误屏蔽
+      // throw this._m.errors.noAC;
     }
     else if (options.bufferSize &&
       !utilities.isPowerOfTwo(options.bufferSize)) {
       throw this._m._errors.notPow2;
     }
     else if (!options.source) {
-      throw this._m._errors.noSource;
+      //因为Linux机器上无法生成AudioContext，因此直接计算，将这类错误屏蔽
+      // throw this._m._errors.noSource;
     }
 
     this._m.audioContext = options.audioContext;
@@ -44,17 +46,17 @@ export class MeydaAnalyzer {
     // TODO: validate options
     this._m.bufferSize = options.bufferSize || this._m.bufferSize || 256;
     this._m.hopSize = options.hopSize || this._m.hopSize || this._m.bufferSize;
-    this._m.sampleRate = options.sampleRate
-      || this._m.audioContext.sampleRate || 44100;
+    this._m.sampleRate = options.sampleRate || /*this._m.audioContext.sampleRate ||*/ 44100;
     this._m.callback = options.callback;
     this._m.windowingFunction = options.windowingFunction || 'hanning';
     this._m.featureExtractors = featureExtractors;
     this._m.EXTRACTION_STARTED = options.startImmediately || false;
 
+    //因为Linux机器上无法生成AudioContext，因此直接计算，将这类错误屏蔽
     //create nodes
-    this._m.spn = this._m.audioContext.createScriptProcessor(
-      this._m.bufferSize, 1, 1);
-    this._m.spn.connect(this._m.audioContext.destination);
+    // this._m.spn = this._m.audioContext.createScriptProcessor(
+    //   this._m.bufferSize, 1, 1);
+    // this._m.spn.connect(this._m.audioContext.destination);
 
     this._m._featuresToExtract = options.featureExtractors || [];
 
@@ -74,41 +76,42 @@ export class MeydaAnalyzer {
     this._m.frame = null;
     this._m.previousFrame = null;
 
-    this.setSource(options.source);
+    //因为Linux机器上无法生成AudioContext，因此直接计算，将这类错误屏蔽
+    // this.setSource(options.source);
 
-    this._m.spn.onaudioprocess = (e)=> {
-      if (this._m.inputData !== null) {
-        this._m.previousInputData = this._m.inputData;
-      }
+    // this._m.spn.onaudioprocess = (e)=> {
+    //   if (this._m.inputData !== null) {
+    //     this._m.previousInputData = this._m.inputData;
+    //   }
 
-      this._m.inputData = e.inputBuffer.getChannelData(0);
+    //   this._m.inputData = e.inputBuffer.getChannelData(0);
 
-      if (!this._m.previousInputData) {
-        var buffer = this._m.inputData;
-      } else  {
-        var buffer = new Float32Array(this._m.previousInputData.length + this._m.inputData.length - this._m.hopSize);
-        buffer.set(this._m.previousInputData.slice(this._m.hopSize));
-        buffer.set(this._m.inputData, this._m.previousInputData.length - this._m.hopSize);
-      };
+    //   if (!this._m.previousInputData) {
+    //     var buffer = this._m.inputData;
+    //   } else  {
+    //     var buffer = new Float32Array(this._m.previousInputData.length + this._m.inputData.length - this._m.hopSize);
+    //     buffer.set(this._m.previousInputData.slice(this._m.hopSize));
+    //     buffer.set(this._m.inputData, this._m.previousInputData.length - this._m.hopSize);
+    //   };
 
-      var frames = utilities.frame(buffer, this._m.bufferSize, this._m.hopSize);
+    //   var frames = utilities.frame(buffer, this._m.bufferSize, this._m.hopSize);
 
-      frames.forEach(f => {
-        this._m.frame = f;
+    //   frames.forEach(f => {
+    //     this._m.frame = f;
 
-        var features = this._m.extract(
-          this._m._featuresToExtract,
-          this._m.frame,
-          this._m.previousFrame);
+    //     var features = this._m.extract(
+    //       this._m._featuresToExtract,
+    //       this._m.frame,
+    //       this._m.previousFrame);
 
-        // call callback if applicable
-        if (typeof this._m.callback === 'function' && this._m.EXTRACTION_STARTED) {
-          this._m.callback(features);
-        }
+    //     // call callback if applicable
+    //     if (typeof this._m.callback === 'function' && this._m.EXTRACTION_STARTED) {
+    //       this._m.callback(features);
+    //     }
 
-        this._m.previousFrame = this._m.frame;
-      });
-    };
+    //     this._m.previousFrame = this._m.frame;
+    //   });
+    // };
   }
 
   /**
